9198ea235a78b1a621d45af05771ca16dffb86f7
Sajjad Abdullah <clashking1545@gmail.com>
2025-02-12 10:58:45 +0600

    update console log message to reflect MERN Backend Template app

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ async function main() {
 	try {
 		await mongoose.connect(config.database_url);
 		server = app.listen(config.port, () => {
-			console.log(`Personal Finance app listening on port ${config.port}`);
+			console.log(`MERN Backend Template app listening on port ${config.port}`);
 		});
 	} catch (err) {
 		console.log(err);
