81917b907e873b64d7f2436de768315ad93607c5
Sajjad Abdullah <clashking1545@gmail.com>
2025-05-22 12:28:03 +0600

    chore: update environment variables and add MT5 manager credentials; configure ESLint

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+module.exports = [
+	{
+		files: ["**/*.js"],
+		languageOptions: {
+			ecmaVersion: "latest",
+			sourceType: "commonjs",
+			globals: {
+				console: "readonly",
+				module: "readonly",
+				require: "readonly",
+				__dirname: "readonly",
+			},
+		},
+
+		plugins: {
+			js: require("@eslint/js"),
+		},
+		rules: {
+			"no-unused-vars": "warn",
+			"no-undef": ["error", { typeof: true }],
+			"no-unused-expressions": "warn",
+			"no-console": "warn",
+			"no-redeclare": "warn",
+			eqeqeq: ["error", "always", { null: "ignore" }],
+		},
+	},
+];
