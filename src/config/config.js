f21372323538c848b10393e24c2c2b314066f0bc
Sajjad Abdullah <clashking1545@gmail.com>
2025-05-22 12:29:56 +0600

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -7,18 +7,6 @@ const config = {
 	database_url: process.env.DATABASE_URL,
 	jwt_access_secret: process.env.JWT_SECRET_KEY,
 
-	// initial manager body.... start
-	Mt5BaseUrl: process.env.MT5_BASE_URL,
-	managerID: process.env.MANAGER_ID,
-	password: process.env.PASSWORD,
-	serverConfig: process.env.SERVER_CONFIG,
-	mt5Token: process.env.MT5_TOKEN,
-	// initial manager body ...... end
-
-	// base urls
-	userManagementUrl: process.env.USER_MANAGEMENT_BASE_URL,
-	riskManagementUrl: process.env.RISK_MANAGEMENT_BASE_URL,
-	emailManagementUrl: process.env.EMAIL_MANAGEMENT_BASE_URL,
 };
 
 module.exports = config;
